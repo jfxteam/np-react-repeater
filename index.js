3dc4cb2d07cec82fb4a8e1e0defa48683f604d48
Антон <antoinedzejob@gmail.com>
2020-01-10 21:11:21 +0300

    prepare

--- index.js
+++ index.js
@@ -47,3 +47,53 @@ module.exports = class Expandable extends JFactory.Module {
 		return $element;
 	}
 }
+
+
+
+
+
+
+document.body.innerHTML = '<div id="root"></div>';  
+
+var frame = class extends React.Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      data: props.data
+    };
+  }
+
+  addRow(e){
+    this.setState({
+      data: this.state.data.concat('')
+    });
+  }
+
+  removeRow(e, index){
+    this.setState({
+      data: this.state.data.filter((item, itemIndex) => itemIndex !== index)
+    })
+  }
+
+  onChange(e, index){
+    this.setState({data: Object.values({...this.state.data, ...{[index]: e.target.value}})})
+  }
+  
+  render(){
+    var rows = this.state.data.map((value, index) => React.createElement(row, {...{index, value}, ...{removeRow: this.removeRow.bind(this), onChange: this.onChange.bind(this)}})),
+      rowsWrapper = React.createElement.apply(null, ['div', {className: 'expandable-rows'}].concat(rows)),
+      addBtn = React.createElement('button', {className: 'add-btn', onClick: this.addRow.bind(this)}, 'Добавить');
+    return React.createElement('div', {className: 'expandable'}, rows, addBtn);
+  }
+}
+
+var row = class extends React.Component {
+  render(){
+    var element = React.createElement('input', {value: this.props.value, name: `person[${this.props.index}]name`, onChange: e => this.props.onChange(e, this.props.index)}),
+      removeBtn = React.createElement('button', {className: 'remove-btn', onClick: e => this.props.removeRow(e, this.props.index)}, 'Удалить');
+    return React.createElement('div', {className: 'expandable-row'}, element, removeBtn);
+  }
+}
+var component = React.createElement(frame, {data: ['Вася','Петя', 'Коля', 'Маша']});
+var el = ReactDOM.render(component, document.getElementById('root'))
